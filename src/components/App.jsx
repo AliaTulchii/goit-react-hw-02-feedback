13828d2150f08c25ed6e648c27fe3ffcc8184270
Halina Tulchii <halendeihalyna@gmail.com>
2023-04-27 15:17:53 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import Feedback from '../components/Feedback/Feedback';
+import {Component} from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      
+import Feedback from './Feedback/Feedback'
+import Section from './Section/Section';
+import Statistics from './Statistics';
 
-      <Feedback />
-      
-    </div>
-  );
+import {FEEDBACK_OPTIONS} from '../data/constans'
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
 };
+
+  handleFeedback = ({ target }) => {
+    const { feedback } = target.dataset;
+    this.setState((prevState) => ({ [feedback]: prevState[feedback] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total ? Math.round((good / total) * 100) : 0;
+  }
+  
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <div>
+      <Section title='Please leave feedback'>
+        <Feedback options={FEEDBACK_OPTIONS} onClickFeedback={this.handleFeedback} />
+      </Section>
+        
+        <Section title='Statistics'>
+          <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />
+        </Section>
+      </div>
+    )
+  }
+}
+
+export default App;
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,95 +1,19 @@
-import {FaRegThumbsUp, FaRegGrimace, FaRegThumbsDown } from "react-icons/fa";
-import React from 'react';
-import css from '../Feedback/Feedback.module.css';
+import PropTypes from 'prop-types'
+import css from './Feedback.module.css'
 
+const FeedbackButton = ({ feedback, onClickFeedback }) => {
+    return (<button type='button' data-feedback={feedback} onClick={onClickFeedback} className={css.Feedback__buttn}>
+        {feedback}
+    </button>)
+}
 
-class Feedback extends React.Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-        total: 0,
-        percentage: 0,
-    };
+const Feedback = ({ options, onClickFeedback }) => {
+    return options.map(options => <FeedbackButton feedback={options} onClickFeedback={onClickFeedback}/>)
+}
 
-    handleIncrementGood = () => {
-        this.setState(prevState => {
-            return {
-                good: prevState.good + 1,
-            };
-        });
-    }
-
-    handleIncrementNeut = () => {
-        this.setState(prevState => {
-            return {
-                neutral: prevState.neutral + 1,
-            };
-        });
-    }
-
-    handleIncrementBad = () => {
-        this.setState(prevState => {
-            return {
-                bad: prevState.bad + 1,
-            };
-        });
-    }
-
-    countTotalFeedback = () => {
-        this.setState(prevState => {
-            return {
-                total: prevState.good + prevState.neutral + prevState.bad ,
-            };
-        });
-    }
-
-    render() {
-        return (
-            <div className={css.Feedback}>
-                <h1>Please leave feedback</h1>
-
-                <div className={css.Feedback__controls}>
-                    <button
-                        type="button"
-                        className={`${css.Feedback__buttnGood} ${css.Feedback__buttn}`}
-                        onClick={this.handleIncrementGood}>
-                        <FaRegThumbsUp  size="16px" className={css.icon} />
-                        Good
-                    </button>
-
-                    <button
-                        type="button"
-                        className={`${css.Feedback__buttnNeutr} ${css.Feedback__buttn}`}
-                        onClick={this.handleIncrementNeut}>
-                        <FaRegGrimace  size="16px" className={css.icon} />
-                        Neutral
-                    </button>
-
-                    <button
-                        type="button"
-                        className={`${css.Feedback__buttnBad} ${css.Feedback__buttn}`}
-                        onClick={this.handleIncrementBad}>
-                        <FaRegThumbsDown  size="16px" className={css.icon} />
-                        Bad
-                    </button>
-                </div>
-                
-                <h2>Statistics</h2>
-
-                <div className={css.Feedback__display}>
-                    <span className={css.Feedback__value}>Good: {this.state.good}</span>
-                    <span className={css.Feedback__value}>Neutral: {this.state.neutral}</span>
-                    <span className={css.Feedback__value}>Bad: {this.state.bad}</span>
-                    <span className={css.Feedback__value}>Total: {this.state.total}</span>
-                    <span className={css.Feedback__value}>Positive feedback: {this.state.percentage}</span>
-                </div>
-
-
-
-             </div>
-        )
-    }
+Feedback.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string),
+    onClickFeedback: PropTypes.func.isRequired,
 }
 
 export default Feedback;
